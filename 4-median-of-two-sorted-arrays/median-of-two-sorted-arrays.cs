145442aa73456c25445972afd0beca9c305713d1
Gustavo <113948322+gustavomaltanv@users.noreply.github.com>
2025-01-03 01:18:55 -0300

    Time: 17 ms (15.88%) | Memory: 56.9 MB (6.60%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cs
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cs
@@ -1,9 +1,12 @@
 public class Solution {
     public double FindMedianSortedArrays(int[] nums1, int[] nums2) {
         int[] arr = nums1.Concat(nums2).OrderBy(x => x).ToArray();
-        int skip = ((arr.Length + (arr.Length % 2)) / 2) - 1;
-        arr = arr.Skip(skip).Take(2 - (arr.Length % 2)).ToArray();
-
-        return (double) arr.Sum() / arr.Length;
+        int length = arr.Length;
+        if ( length % 2 == 0) 
+        {
+            return (arr[ (length / 2) - 1 ] + arr[ length / 2 ]) / 2.0; 
+            
+        }
+        return (double)  arr[length/2];
     }
 }
