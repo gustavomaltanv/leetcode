826a5ffc7bdf460835be379e7cfbc7227232585b
Gustavo <113948322+gustavomaltanv@users.noreply.github.com>
2025-01-06 00:03:51 -0300

    Time: 1 ms (99.75%) | Memory: 47.8 MB (94.11%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.cs
+++ 13-roman-to-integer/roman-to-integer.cs
@@ -1,28 +1,28 @@
 public class Solution {
     public int RomanToInt(string s) {
         int value = 0;
-        Dictionary<char,int> romans = RomanValues();
         int last = 0;
         for(int i = s.Length - 1; i >= 0; i--) {
-            if(last <= romans[s[i]] ) {
-                value += romans[s[i]];
+            if(last <= GetIntByValue(s[i]) ) {
+                value += GetIntByValue(s[i]);
             } else {
-                value -= romans[s[i]];
+                value -= GetIntByValue(s[i]);
             }
-            last = romans[s[i]];
+            last = GetIntByValue(s[i]);
         }
         return value;
     }
 
-    public Dictionary<char, int> RomanValues() {
-        var roman = new Dictionary<char, int>();
-        roman.Add(char.Parse("I"), 1);
-        roman.Add(char.Parse("V"), 5);
-        roman.Add(char.Parse("X"), 10);
-        roman.Add(char.Parse("L"), 50);
-        roman.Add(char.Parse("C"), 100);
-        roman.Add(char.Parse("D"), 500);
-        roman.Add(char.Parse("M"), 1000);
-        return roman;
+    public int GetIntByValue(char c) {
+        switch(c) {
+            case 'I': return 1; break;
+            case 'V': return 5; break;
+            case 'X': return 10; break;
+            case 'L': return 50; break;
+            case 'C': return 100; break;
+            case 'D': return 500; break;
+            case 'M': return 1000; break;
+            default: return 0; break;
+        }
     }
 }
