d314d614d70a798426e03cab368bad89ac91a58f
Gustavo <113948322+gustavomaltanv@users.noreply.github.com>
2025-01-09 22:53:09 -0300

    Time: 6 ms (64.86%) | Memory: 52.9 MB (57.21%) - LeetSync

--- 459-repeated-substring-pattern/repeated-substring-pattern.cs
+++ 459-repeated-substring-pattern/repeated-substring-pattern.cs
@@ -1,13 +1,9 @@
 public class Solution {
     public bool RepeatedSubstringPattern(string s) {
-        int n = s.Length / 2;
-        System.Text.StringBuilder substring = new StringBuilder("", n);
-        for(int i = 0; i < n; i++)
-        {
-            substring.Append( s[i] );
-            if( s.Replace(substring.ToString(), "") == "" ) return true;
-        }
+        string doubled = s + s;
 
-        return false;
+        string reduced = doubled.Substring(1, doubled.Length - 2);
+
+        return reduced.Contains(s);
     }
 }
