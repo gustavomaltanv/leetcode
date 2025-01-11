27e03e17e63fe7c2f141ba4ae291903bfd0f0b6c
Gustavo <113948322+gustavomaltanv@users.noreply.github.com>
2025-01-11 13:15:03 -0300

    Time: 45 ms (39.39%) | Memory: 77.4 MB (40.91%) - LeetSync

--- 1742-widest-vertical-area-between-two-points-containing-no-points/widest-vertical-area-between-two-points-containing-no-points.cs
+++ 1742-widest-vertical-area-between-two-points-containing-no-points/widest-vertical-area-between-two-points-containing-no-points.cs
@@ -1,6 +1,6 @@
 public class Solution {
     public int MaxWidthOfVerticalArea(int[][] points) {
-        //points = points.OrderBy(a => a[0]).ToArray();
+        
         int[] pointsX = new int[points.Length];
         
         for(int i = 0; i < points.Length; i++)
