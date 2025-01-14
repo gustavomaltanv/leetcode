4693ff766b2cf6de6e529a1903f004d6891bd88b
Gustavo <113948322+gustavomaltanv@users.noreply.github.com>
2025-01-13 21:14:49 -0300

    Time: 0 ms (100.00%) | Memory: 43.4 MB (14.02%) - LeetSync

--- 2341-count-prefixes-of-a-given-string/count-prefixes-of-a-given-string.cs
+++ 2341-count-prefixes-of-a-given-string/count-prefixes-of-a-given-string.cs
@@ -2,9 +2,9 @@ public class Solution {
     public int CountPrefixes(string[] words, string s) {
         int answer = 0;
         foreach(string word in words) {
-            if( word.Length <= s.Length )
+            if( word.Length <= s.Length && s.Substring(0, word.Length) == word )
             {
-                if( s.StartsWith(word) ) answer++;
+                answer++;
             }
         }
         return answer;
