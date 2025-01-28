515138b0da6407898c4c9bea8422e5133be98c61
Gustavo <113948322+gustavomaltanv@users.noreply.github.com>
2025-01-27 22:07:06 -0300

    Time: 0 ms (100.00%) | Memory: 41.9 MB (68.89%) - LeetSync

--- 3136-valid-word/valid-word.cs
+++ 3136-valid-word/valid-word.cs
@@ -1,32 +1,19 @@
 public class Solution {
     public bool IsValid(string word) {
-        return word.Length >= 3 && hasVowel(word) && hasConsonant(word) && !hasSpecialChar(word);
+        return word.Length >= 3 && Validation(word);
     }
 
-    public bool hasVowel(string word) {
-        foreach (char c in word) {
-            if( "aeiouAEIOU".IndexOf(c) >= 0 )
-                return true; 
-        }
-        return false;
-    }
-
-    public bool hasConsonant(string word) {
-        foreach (char c in word) {
-            if( Char.IsLetter(c) && "aeiouAEIOU".IndexOf(c) < 0 )
-                return true; 
-        }
-        return false;
-    }
-
-    public bool hasSpecialChar(string word) {
-        foreach (char c in word) {
-            if(!Char.IsLetter(c) && !Char.IsNumber(c) ) {
-                return true;
+    public bool Validation(string word) {
+        bool vowel = false, consonant = false;
+        foreach(char c in word) {
+            if(Char.IsLetter(c)) {
+                if( "aeiouAEIOU".IndexOf(c) >= 0 ) {vowel = true;}
+                else {consonant = true;}
+            }
+            else if(!Char.IsNumber(c)) {
+                return false;
             }
         }
-        return false;
+        return vowel && consonant;
     }
-
-
 }
