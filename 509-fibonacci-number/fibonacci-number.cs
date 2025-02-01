4b3be33af40f1ec516b1e2cc98ed14e1afec52a9
Gustavo <113948322+gustavomaltanv@users.noreply.github.com>
2025-02-01 17:45:40 -0300

    Time: 26 ms (56.34%) | Memory: 28.9 MB (17.90%) - LeetSync

--- 509-fibonacci-number/fibonacci-number.cs
+++ 509-fibonacci-number/fibonacci-number.cs
@@ -1,6 +1,18 @@
 public class Solution {
     public int Fib(int n) {
         if(n <= 1) return n;
+        int n1 = 0, n2 = 1;
+        while (n > 1) {
+            int sum = n1 + n2;
+            n1 = n2;
+            n2 = sum;
+            n--;
+        }
+        return n2;
+
+        /* Resposta com recursão
+        if(n <= 1) return n;
         return Fib(n-1) + Fib(n-2);
+        */
     }
 }
