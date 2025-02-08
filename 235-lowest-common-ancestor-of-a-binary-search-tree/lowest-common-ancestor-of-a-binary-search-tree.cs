de71f09f391effbc410b7e34bca1518776e442f6
Gustavo <113948322+gustavomaltanv@users.noreply.github.com>
2025-02-08 20:24:55 -0300

    Time: 100 ms (88.73%) | Memory: 49.4 MB (81.48%) - LeetSync

--- 235-lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.cs
+++ 235-lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.cs
@@ -10,8 +10,15 @@
 
 public class Solution {
     public TreeNode LowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        if(p.val > root.val && q.val > root.val) return LowestCommonAncestor(root.right, p, q);
-        else if(p.val < root.val && q.val < root.val) return LowestCommonAncestor(root.left, p, q);
-        else return root;
+        while (root != null) {
+            if(p.val > root.val && q.val > root.val) {
+                root = root.right;
+            }
+            else if(p.val < root.val && q.val < root.val) {
+                root = root.left;
+            }
+            else return root;
+        }
+        return null;
     }
 }
