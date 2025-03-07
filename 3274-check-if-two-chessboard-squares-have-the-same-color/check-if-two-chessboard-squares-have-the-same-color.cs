d2e96e2b6ecea24d9f306a6e319eafc0914d0464
Gustavo <113948322+gustavomaltanv@users.noreply.github.com>
2025-03-06 23:24:20 -0300

    Time: 70 ms (24.24%) | Memory: 43.1 MB (28.79%) - LeetSync

--- 3274-check-if-two-chessboard-squares-have-the-same-color/check-if-two-chessboard-squares-have-the-same-color.cs
+++ 3274-check-if-two-chessboard-squares-have-the-same-color/check-if-two-chessboard-squares-have-the-same-color.cs
@@ -2,8 +2,7 @@ public class Solution {
     public bool CheckTwoChessboards(string coordinate1, string coordinate2) {
         int x1 = (int) coordinate1[0], y1 = coordinate1[1];
         int x2 = (int) coordinate2[0], y2 = coordinate2[1]; 
-        return (IsWhiteTile(x1, y1) && IsWhiteTile(x2, y2)) 
-        || (!IsWhiteTile(x1, y1) && !IsWhiteTile(x2, y2));
+        return (IsWhiteTile(x1, y1) == IsWhiteTile(x2, y2));
     }
 
     private static bool IsWhiteTile (int x, int y) {
