9d53633d53f7e2178fa605e76c2bdaa930ba7ae9
Gustavo <113948322+gustavomaltanv@users.noreply.github.com>
2025-03-20 22:48:04 -0300

    Time: 31 ms (95.55%) | Memory: 29 MB (69.86%) - LeetSync

---
+++ 190-reverse-bits/reverse-bits.cs
@@ -0,0 +1,10 @@
+public class Solution {
+    public uint reverseBits(uint n) {
+        uint ans = 0;
+        for(int i = 0; i < 32; i++) {
+            ans = (ans << 1) | (n & 1);
+            n = n >> 1; 
+        } 
+        return ans;
+    }
+}
